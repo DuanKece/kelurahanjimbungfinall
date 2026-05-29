d982fb5a63009a927acb178d594d65c51887f1a8
DuanKece <akunytgaming0011@gmail.com>
2026-05-29 10:56:03 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,26 +1,3 @@
-const btn = document.getElementById("theme-toggle");
-const icon = btn.querySelector("i");
-
-btn.addEventListener("click", () => {
-
-    // fade out dulu
-    icon.style.opacity = "0";
-
-        setTimeout(() => {
-    if (icon.classList.contains("fa-moon")) {
-        icon.classList.remove("fa-moon");
-        icon.classList.add("fa-sun");
-    } else {
-        icon.classList.remove("fa-sun");
-        icon.classList.add("fa-moon");
-    }
-
-    // fade in lagi
-        icon.style.opacity = "1";
-
-    }, 150);
-});
-
 let lastScroll = 0;
 const navbar = document.querySelector(".navbar-wrapper");
 
@@ -28,12 +5,17 @@ window.addEventListener("scroll", () => {
     let currentScroll = window.pageYOffset;
 
     if (currentScroll > lastScroll) {
-        // scroll ke bawah → sembunyikan
         navbar.classList.add("navbar-hide");
     } else {
-        // scroll ke atas → tampilkan
         navbar.classList.remove("navbar-hide");
     }
 
     lastScroll = currentScroll;
+});
+
+const navToggle = document.getElementById("navToggle");
+const navbarMenu = document.getElementById("navbarMenu");
+
+navToggle.addEventListener("click", () => {
+    navbarMenu.classList.toggle("active");
 });
